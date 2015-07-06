80df877e52e8a1c9a81486b9f13b7c355e23435b
sq_xu <sq_xu@Ctrip.com>
2015-07-06 15:58:23 +0800

    file write

--- NodeJs/NodeServer/httpGetServer.js
+++ NodeJs/NodeServer/httpGetServer.js
@@ -4,17 +4,51 @@ var server = new http.Server();
 
 var url = require('url');
 var util = require('util');
+// 加载File System读写模块
+var fs = require('fs');
+// 加载编码转换模块
+var iconv = require('iconv-lite');
+
+function writeFile(file , str){
+    // appendFile，如果文件不存在，会自动创建新文件
+    // 如果用writeFile，那么会删除旧文件，直接写新文件
+    fs.appendFileSync(file, str, {"encoding": "utf-8"},function(err){
+        if(err)
+            console.log("fail " + err);
+        else
+            console.log("写入文件ok");
+    });
+}
+
+function readFile(file){
+    fs.readFile(file,"utf8", function(err, data){
+        if(err)
+            console.log("读取文件fail " + err);
+        else{
+            // 读取成功时
+            // 输出字节数组
+            console.log(data);
+            // 把数组转换为gbk中文
+            var str = iconv.decode(data, 'gbk');
+            console.log(str);
+        }
+    });
+}
+
 
 server.on('request', function(req, res){
-    console.log(req.data);
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+   // console.log(req.data);
+    res.writeHead(200, {'Content-Type': 'text/plain', 'charset': 'utf-8'});
     var reqObj =url.parse(req.url, true);
+    var strval= "";
     //获取get传进来的参数 param
-    if(reqObj.query && reqObj.query.param){
-        res.write(reqObj.query.param);
+    for (var e in reqObj.query){
+    	res.write(reqObj.query[e]);
+        strval += e + ":" + reqObj.query[e] + " ";
     }
+    writeFile('a.txt', strval + "\n");
     res.end(util.inspect(reqObj)); //res.end结束并发送
 });
 server.listen(4000);
 
-console.log("HTTP server is listening at port 4000.")
+console.log("HTTP server is listening at port 4000.");
--- NodeJs/NodeServer/httpServer.js
+++ NodeJs/NodeServer/httpServer.js
@@ -11,6 +11,20 @@ var url = require('url');
 var util = require('util');
 
 server.on('request', function(req, res){
+
+   /* req.on('data', function(chunk){//请求数据到来时触发该事件 不监听 ，请求体即被抛弃
+        post += chunk;
+        console.log(post);
+    });
+
+    req.on('end', function(){ //end 请求数据传送完时，触发该事件
+        post = querystr.parse(post);
+        console.log(post.name);
+        console.log(post.email);//！！important 获取相应的具体参数
+        res.write(util.inspect(post) , 'utf-8');
+        res.end(util.inspect(post));//end 结束并发送
+    });*/
+
 	console.log(req.data);
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.write('<h1>Node.js111</h1>');
